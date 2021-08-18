cee548b04a986b5dd0a3a3beda10b7c4c0843491
Tan Le Minh <v.tanlm2@vinfast.vn>
2021-08-18 21:39:01 +0700

    add bar to session

--- 02_session/main.go
+++ 02_session/main.go
@@ -24,7 +24,25 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
+	http.HandleFunc("/bar", bar)
 	http.ListenAndServe(":8080", nil)
+}
+
+func bar(w http.ResponseWriter, req *http.Request) {
+	c, err := req.Cookie("session")
+	if err != nil {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	un, ok := dbSessions[c.Value]
+	if !ok {
+		http.Redirect(w, req, "/", http.StatusSeeOther)
+		return
+	}
+
+	u := dbUsers[un]
+	tpl.ExecuteTemplate(w, "bar.gohtml", u)
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
